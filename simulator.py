1e4e1d741a5add2704446f29496df61ddefb0799
jb3rndt <berndtjannis@gmail.com>
2024-06-05 17:02:17 +0200

    Simplify GPS

--- simulator.py
+++ simulator.py
@@ -38,60 +38,57 @@ class QueueingSimulator:
 
 class GPSSimulator(QueueingSimulator):
     def run(self):
-        next_packet_time = self.packet_arrivals[0].time
-
         # Serve packets until no more packets will arrive and all queues are empty
         while len(self.packet_arrivals) > 0 or any(
             len(queue) > 0 for queue in self.flow_queues.values()
         ):
             self.enqueue_packets()
 
-            next_packet_time = (
-                self.packet_arrivals[0].time if len(self.packet_arrivals) > 0 else None
+            # Send one bit of the first packet in each flow in round-robin fashion
+            # Skip the time forward until the next packet arrives or one packet finishes
+            number_of_active_flows = len(
+                [1 for queue in self.flow_queues.values() if len(queue) > 0]
             )
+            time_until_next_packet = (
+                self.packet_arrivals[0].time - self.time
+                if len(self.packet_arrivals) > 0
+                else sys.maxsize
+            )
+            fully_skippable_rounds = time_until_next_packet // number_of_active_flows
+            # Find the minimum remaining size of the first packet in each flow
+            # to determine how many rounds can be skipped. The size is reduced by 1,
+            # so the packet will finish in the next round (which will not be squashed then).
+            skippable_rounds_until_packet_finishes = min(
+                max(queue[0].remaining_size - 1, 0)
+                for queue in self.flow_queues.values()
+                if len(queue) > 0
+            )
+            skippable_rounds = min(
+                fully_skippable_rounds, skippable_rounds_until_packet_finishes
+            )
+            if skippable_rounds == 0:
+                for queue in self.flow_queues.values():
+                    # Always check for new packets
+                    self.enqueue_packets()
 
-            # Send bits until the next packet arrives or all packets finished
-            while (next_packet_time is None or self.time < next_packet_time) and any(
-                len(queue) > 0 for queue in self.flow_queues.values()
+                    if len(queue) > 0:
+                        self.time += 1 / self.data_rate
+                        queue[0].remaining_size -= 1
+                        if queue[0].remaining_size == 0:
+                            self.update_metrics(queue.pop(0))
+            else:
+                self.time += (
+                    skippable_rounds / self.data_rate
+                ) * number_of_active_flows
+                for queue in self.flow_queues.values():
+                    if len(queue) > 0:
+                        queue[0].remaining_size -= skippable_rounds
+
+            # If all packets finished, move time to the next arriving packet
+            if len(self.packet_arrivals) > 0 and all(
+                len(queue) == 0 for queue in self.flow_queues.values()
             ):
-                # Send one bit of the first packet in each flow in round-robin fashion
-                # Skip the time forward until the next packet arrives or one packet finishes
-                number_of_active_flows = len(
-                    [1 for queue in self.flow_queues.values() if len(queue) > 0]
-                )
-                time_until_next_packet = (next_packet_time or sys.maxsize) - self.time
-                fully_skippable_rounds = (
-                    time_until_next_packet // number_of_active_flows
-                )
-                skippable_rounds_until_packet_finishes = min(
-                    max(queue[0].remaining_size - 1, 0)
-                    for queue in self.flow_queues.values()
-                    if len(queue) > 0
-                )
-                skippable_rounds = min(
-                    fully_skippable_rounds, skippable_rounds_until_packet_finishes
-                )
-                if skippable_rounds == 0:
-                    for queue in self.flow_queues.values():
-                        # Always check for new packets
-                        self.enqueue_packets()
-
-                        if len(queue) > 0:
-                            self.time += 1 / self.data_rate
-                            queue[0].remaining_size -= 1
-                            if queue[0].remaining_size == 0:
-                                self.update_metrics(queue.pop(0))
-                else:
-                    self.time += (
-                        skippable_rounds / self.data_rate
-                    ) * number_of_active_flows
-                    for queue in self.flow_queues.values():
-                        if len(queue) > 0:
-                            queue[0].remaining_size -= skippable_rounds
-
-            # If all packets finished, move time to the next packet time
-            if next_packet_time is not None and next_packet_time > self.time:
-                self.time = next_packet_time
+                self.time = self.packet_arrivals[0].time
 
     def __str__(self):
         return f"GPS"
